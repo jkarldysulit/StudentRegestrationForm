2244d865c6a129087a65a60994cfd13b94cfabd6
Mephisto <Mephisto@DESKTOP-8SDV07R>
2026-02-28 18:31:55 +0800

    Add project files.

---
+++ StudentRegestrationForm/StudentRegistrationForm.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace StudentRegestrationForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.lblMiddleName = new System.Windows.Forms.Label();
+            this.txtBoxMiddleName = new System.Windows.Forms.TextBox();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.lblDOB = new System.Windows.Forms.Label();
+            this.cmbDay = new System.Windows.Forms.ComboBox();
+            this.cmbMonth = new System.Windows.Forms.ComboBox();
+            this.cmbYear = new System.Windows.Forms.ComboBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(272, 30);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Student Registration Form";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Location = new System.Drawing.Point(13, 60);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(101, 21);
+            this.lblLastName.TabIndex = 1;
+            this.lblLastName.Text = "Last Name *";
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(17, 84);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(355, 29);
+            this.textBoxLastName.TabIndex = 2;
+            this.textBoxLastName.TextChanged += new System.EventHandler(this.textBoxLastName_TextChanged);
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstName.Location = new System.Drawing.Point(13, 116);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(103, 21);
+            this.lblFirstName.TabIndex = 3;
+            this.lblFirstName.Text = "First Name *";
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(17, 140);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(355, 29);
+            this.textBoxFirstName.TabIndex = 4;
+            this.textBoxFirstName.TextChanged += new System.EventHandler(this.textBoxFirstName_TextChanged);
+            // 
+            // lblMiddleName
+            // 
+            this.lblMiddleName.AutoSize = true;
+            this.lblMiddleName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMiddleName.Location = new System.Drawing.Point(13, 172);
+            this.lblMiddleName.Name = "lblMiddleName";
+            this.lblMiddleName.Size = new System.Drawing.Size(125, 21);
+            this.lblMiddleName.TabIndex = 5;
+            this.lblMiddleName.Text = "Middle Name *";
+            // 
+            // txtBoxMiddleName
+            // 
+            this.txtBoxMiddleName.Location = new System.Drawing.Point(17, 196);
+            this.txtBoxMiddleName.Name = "txtBoxMiddleName";
+            this.txtBoxMiddleName.Size = new System.Drawing.Size(355, 29);
+            this.txtBoxMiddleName.TabIndex = 6;
+            this.txtBoxMiddleName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGender.Location = new System.Drawing.Point(13, 237);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(76, 21);
+            this.lblGender.TabIndex = 7;
+            this.lblGender.Text = "Gender *";
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rbMale.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbMale.Location = new System.Drawing.Point(95, 237);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(61, 25);
+            this.rbMale.TabIndex = 8;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = true;
+            this.rbMale.CheckedChanged += new System.EventHandler(this.rbMale_CheckedChanged);
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rbFemale.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbFemale.Location = new System.Drawing.Point(163, 237);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(77, 25);
+            this.rbFemale.TabIndex = 9;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            this.rbFemale.CheckedChanged += new System.EventHandler(this.rbFemale_CheckedChanged);
+            // 
+            // lblDOB
+            // 
+            this.lblDOB.AutoSize = true;
+            this.lblDOB.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDOB.Location = new System.Drawing.Point(17, 262);
+            this.lblDOB.Name = "lblDOB";
+            this.lblDOB.Size = new System.Drawing.Size(114, 21);
+            this.lblDOB.TabIndex = 10;
+            this.lblDOB.Text = "Date of birth*";
+            // 
+            // cmbDay
+            // 
+            this.cmbDay.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmbDay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbDay.FormattingEnabled = true;
+            this.cmbDay.Location = new System.Drawing.Point(12, 297);
+            this.cmbDay.Name = "cmbDay";
+            this.cmbDay.Size = new System.Drawing.Size(90, 29);
+            this.cmbDay.TabIndex = 11;
+            this.cmbDay.SelectedIndexChanged += new System.EventHandler(this.cmbDay_SelectedIndexChanged);
+            // 
+            // cmbMonth
+            // 
+            this.cmbMonth.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmbMonth.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbMonth.FormattingEnabled = true;
+            this.cmbMonth.Location = new System.Drawing.Point(122, 297);
+            this.cmbMonth.Name = "cmbMonth";
+            this.cmbMonth.Size = new System.Drawing.Size(90, 29);
+            this.cmbMonth.TabIndex = 12;
+            this.cmbMonth.SelectedIndexChanged += new System.EventHandler(this.cmbMonth_SelectedIndexChanged);
+            // 
+            // cmbYear
+            // 
+            this.cmbYear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmbYear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbYear.FormattingEnabled = true;
+            this.cmbYear.Location = new System.Drawing.Point(228, 297);
+            this.cmbYear.Name = "cmbYear";
+            this.cmbYear.Size = new System.Drawing.Size(100, 29);
+            this.cmbYear.TabIndex = 13;
+            this.cmbYear.SelectedIndexChanged += new System.EventHandler(this.cmbYear_SelectedIndexChanged);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.Crimson;
+            this.btnRegister.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.ForeColor = System.Drawing.Color.White;
+            this.btnRegister.Location = new System.Drawing.Point(12, 353);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(228, 45);
+            this.btnRegister.TabIndex = 14;
+            this.btnRegister.Text = "Register student";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 413);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.cmbYear);
+            this.Controls.Add(this.cmbMonth);
+            this.Controls.Add(this.cmbDay);
+            this.Controls.Add(this.lblDOB);
+            this.Controls.Add(this.rbFemale);
+            this.Controls.Add(this.rbMale);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.txtBoxMiddleName);
+            this.Controls.Add(this.lblMiddleName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Student Registration";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.Label lblMiddleName;
+        private System.Windows.Forms.TextBox txtBoxMiddleName;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.Label lblDOB;
+        private System.Windows.Forms.ComboBox cmbDay;
+        private System.Windows.Forms.ComboBox cmbMonth;
+        private System.Windows.Forms.ComboBox cmbYear;
+        private System.Windows.Forms.Button btnRegister;
+    }
+}
+
---
+++ StudentRegestrationForm/StudentRegistrationForm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace StudentRegestrationForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int day = 1; day <= 31; day++)
+            {
+                cmbDay.Items.Add(day);
+            }
+            cmbDay.Text = "-Day-";
+
+            string[] months = { "January", "February", "March", "April", "May", "June",
+                                 "July", "August", "September", "October", "November", "December" };
+            foreach (string month in months)
+            {
+                cmbMonth.Items.Add(month);
+            }
+            cmbMonth.Text = "-Month-";
+
+            for (int year = 1940; year <= DateTime.Now.Year; year++)
+            {
+                cmbYear.Items.Add(year);
+            }
+            cmbYear.Text = "-Year-";
+        }
+
+        private void textBoxLastName_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBoxFirstName_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void rbMale_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void rbFemale_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void cmbDay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void cmbMonth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void cmbYear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string lastName = textBoxLastName.Text.Trim();
+            string firstName = textBoxFirstName.Text.Trim();
+            string middleName = txtBoxMiddleName.Text.Trim();
+
+            foreach (char c in lastName + firstName + middleName)
+            {
+                if (char.IsDigit(c))
+                {
+                    MessageBox.Show("Invalid input! Names should not contain numbers.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                MessageBox.Show("Please enter your Last Name.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxLastName.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                MessageBox.Show("Please enter your First Name.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxFirstName.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(middleName))
+            {
+                MessageBox.Show("Please enter your Middle Name.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtBoxMiddleName.Focus();
+                return;
+            }
+
+            if (!rbMale.Checked && !rbFemale.Checked)
+            {
+                MessageBox.Show("Please select a Gender.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (cmbDay.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a Day.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (cmbMonth.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a Month.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (cmbYear.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a Year.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                int day = cmbDay.SelectedIndex + 1;
+                int month = cmbMonth.SelectedIndex + 1;
+                int year = int.Parse(cmbYear.SelectedItem.ToString());
+                DateTime testDate = new DateTime(year, month, day);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid date! Please check the Day, Month, and Year.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string gender = rbMale.Checked ? "Male" : "Female";
+
+            string dobDay = cmbDay.SelectedItem.ToString();
+            string dobMonth = cmbMonth.SelectedItem.ToString();
+            string dobYear = cmbYear.SelectedItem.ToString();
+            string dob = dobDay + "/" + dobMonth + "/" + dobYear;
+
+            string message = "Student name: " + firstName + " " + middleName + " " + lastName + "\n" +
+                             "Gender: " + gender + "\n" +
+                             "Date of birth: " + dob;
+
+            MessageBox.Show(message, "Student Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
